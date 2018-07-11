61b7336231cdcc71f058d09781e7c875108c0061
ekliot <ekliot@gmail.com>
2018-07-11 16:40:28 -0400

    actually add the decoupled UI...

---
+++ battles/UI/battle_ui.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+# ========== #
+# UI CONTROL #
+# ========== #
+
+# # duel area
+
+# hero river
+# enemy river
+# timer
+# river swapping
+
+# # card area
+
+# end turn
+# dragging cards
+# mulligan
+# deck
+# hand
---
+++ battles/battle_params.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var PARAMS = {
+  'player_data': {
+    'hero': null
+  },
+  'enemy_data': {
+    'root': null,
+    'minions': {}
+  }
+}
