0c8913a0e0e894ba9e6d1c55d4bcefa0ebf28e5d
ekliot <ekliot@gmail.com>
2018-10-14 17:12:49 -0400

    fix river step icon mouse locking

--- battles/ui/rivers/river_step_ui.gd
+++ battles/ui/rivers/river_step_ui.gd
@@ -1,7 +1,7 @@
 extends CenterContainer
 
-signal pointer_lockon # lock_position, river_step
-signal pointer_unlock
+signal pointer_lockon(pos, river)
+signal pointer_unlock()
 
 var MODEL = null
 var MOMENTUM_LEVEL = -1 setget ,get_momentum
@@ -11,11 +11,16 @@ onready var active_card_icon = $Icon
 
 var hovered = false setget ,is_hovered
 
-# == OVERRIDES == #
+
+"""
+==== OVERRIDES
+"""
+
 
 func _ready():
   connect('mouse_entered', self, '_pointer_lockon')
   connect('mouse_exited', self, '_pointer_unlock')
+
 
 func _input(ev):
   if ev is InputEventMouseMotion:
@@ -27,7 +32,11 @@ func _input(ev):
       hovered = false
       _pointer_unlock()
 
-# == SIGNALS == #
+
+"""
+==== SIGNALS
+"""
+
 
 func connect_to_model(step_model):
   MODEL = step_model
@@ -36,68 +45,82 @@ func connect_to_model(step_model):
   MODEL.connect('card_cleared', self, '_on_model_card_cleared')
   MODEL.connect('card_activated', self, '_on_model_card_activated')
 
+
 func _on_model_card_placed(card):
   clear_card()
   active_card = card
   replace_icon(card.get_icon())
 
+
 func _on_model_card_cleared(card):
   clear_card()
+
 
 func _on_model_card_activated(card):
   # TODO fancy graphical effects
   pass
 
+
 # NOTE for some reason, these signals are being emitted twice if the child
 # TextureRect mouse filter is set to Ignore. This must be done, or else the
 # child will block mouse events (even if set to pass)
 
+
 func _pointer_lockon():
   emit_signal('pointer_lockon', get_pointer_lock(), self)
+
 
 func _pointer_unlock():
   emit_signal('pointer_unlock')
 
-# == CORE == #
+
+"""
+==== CORE
+"""
+
 
 func valid_for(card):
   return MODEL.valid_for(card)
 
+
 func clear_card():
   if is_active():
     active_card = null
-    active_card_icon.queue_free()
+    active_card_icon.texture = null
+
 
 func replace_icon(texture):
-  if active_card_icon:
-    active_card_icon.queue_free()
+  $Icon.texture = texture
 
-  # TODO make CardIcon scene instead of using TextureRect
-  var trect = TextureRect.new()
-  trect.texture = texture
-  add_child(trect)
-  trect.name = "Icon"
-  active_card_icon = trect
 
-# == GETTERS == #
+"""
+==== GETTERS
+"""
+
 
 func is_active():
   return active_card != null
 
+
 func is_hovered():
   return hovered
+
 
 func get_river_id():
   return MODEL.RIVER.RIVER_ID
 
+
 func get_active_card():
   return active_card
+
 
 func get_momentum():
   return MOMENTUM_LEVEL
 
+
 func get_center():
   return rect_size / 2
+
 
 func get_pointer_lock():
   return rect_global_position + Vector2(rect_size.x / 2, rect_size.y)
