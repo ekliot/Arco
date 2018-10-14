e72994748d10bcf447917f10564ccc0f709c3cea
ekliot <ekliot@gmail.com>
2018-10-14 14:07:20 -0400

    effect building

---
+++ cards/effect.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const NAME = ""
+const DESCRIPTION = ""
+const SUIT_PRIMARY = null
+const SUIT_SECONDARY = null
+
+func _init(_name, _desc, suit_prim, suit_sec):
+  pass
+
+func on_activate(a, b, c):
+  pass
+
+func on_trigger(a, b, c):
+  pass
+
+func on_play(a, b, c):
+  pass
